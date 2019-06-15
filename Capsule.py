28841d7db7c1d51ef78f981becf0502277923209
Vasileios Charatsidis <charatsidisvasileios@gmail.com>
2019-06-15 15:03:22 +0200

    refactoring

--- Capsule.py
+++ Capsule.py
@@ -10,19 +10,14 @@ def squash(x, axis=-1):
 
 
 class Capsule(Layer):
-    def __init__(self, num_capsule, dim_capsule, routings=3, kernel_size=(9, 1), share_weights=True,
-                 activation='default', **kwargs):
+    def __init__(self, num_capsule, dim_capsule, routings=3, kernel_size=(9, 1), share_weights=True, **kwargs):
         super(Capsule, self).__init__(**kwargs)
         self.num_capsule = num_capsule
         self.dim_capsule = dim_capsule
         self.routings = routings
         self.kernel_size = kernel_size
         self.share_weights = share_weights
-
-        if activation == 'default':
-            self.activation = squash
-        else:
-            self.activation = Activation(activation)
+        self.squash = squash
 
     def build(self, input_shape):
         super(Capsule, self).build(input_shape)
@@ -43,7 +38,7 @@ class Capsule(Layer):
                                      initializer='glorot_uniform',
                                      trainable=True)
 
-    def call(self, u_vecs):
+    def routing(self, u_vecs):
         if self.share_weights:
             u_hat_vecs = K.conv1d(u_vecs, self.W)
         else:
@@ -51,27 +46,26 @@ class Capsule(Layer):
 
         batch_size = K.shape(u_vecs)[0]
         input_num_capsule = K.shape(u_vecs)[1]
-        u_hat_vecs = K.reshape(u_hat_vecs, (batch_size, input_num_capsule,
-                                            self.num_capsule, self.dim_capsule))
+        u_hat_vecs = K.reshape(u_hat_vecs, (batch_size, input_num_capsule, self.num_capsule, self.dim_capsule))
         u_hat_vecs = K.permute_dimensions(u_hat_vecs, (0, 2, 1, 3))
         # final u_hat_vecs.shape = [None, num_capsule, input_num_capsule, dim_capsule]
 
         b = K.zeros_like(u_hat_vecs[:, :, :, 0])  # shape = [None, num_capsule, input_num_capsule]
         for i in range(self.routings):
-            #b = K.permute_dimensions(b, (0, 2, 1))  # shape = [None, input_num_capsule, num_capsule]
             c = K.softmax(b)
-
-            #c = K.permute_dimensions(c, (0, 2, 1))
-            #b = K.permute_dimensions(b, (0, 2, 1))
-
             s = K.batch_dot(c, u_hat_vecs, [2, 2])
-            v = self.activation(s)
+            v = self.squash(s)
 
             if i < self.routings - 1:
                 b = b + K.batch_dot(v, u_hat_vecs, [2, 3])
 
-        return v
+        poses = v
+        activations = K.sqrt(K.sum(K.square(poses), 2))
+        return poses, activations
 
+    def call(self, u_vecs):
+        poses, _ = self.routing(u_vecs)
+        return poses
 
     def compute_output_shape(self, input_shape):
         return (None, self.num_capsule, self.dim_capsule)
--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from keras.models import Model
 from sklearn.metrics import log_loss
 from keras.layers import Bidirectional, Dense, Dropout, Embedding, Flatten
 from keras.layers import GRU, Input
+import losses
 
 from Capsule import Capsule
 
@@ -15,49 +16,52 @@ Num_capsule = 10
 Dim_capsule = 16
 dropout_p = 0.3
 rate_drop_dense = 0.3
+number_classes = 6
 
 
-def train_model(model, batch_size, train_x, train_y, val_x, val_y):
-    patience = 30
+def numeric_to_one_hot(test_y):
+    onehot_encoded = []
+    for value in test_y:
+        letter = [0 for _ in range(number_classes)]
+        letter[int(value)] = 1
+        onehot_encoded.append(letter)
+
+    return np.array(onehot_encoded)
+
+
+def calc_accuracy(predictions, numeric_test_y):
+    preds = np.argmax(predictions, 1)
+    # print(preds)
+    # print(numeric_val_y)
+    result = preds == numeric_test_y
+    sum = np.sum(result)
+    accuracy = sum / float(len(preds))
+
+    return accuracy
+
+
+def train_model(model, batch_size, train_x, train_y, test_x, test_y):
+    patience = 20
     best_loss = -1
     best_weights = None
     best_epoch = 0
 
-    onehot_encoded = []
-    numeric_val_y = val_y
-    print(numeric_val_y.shape)
-    for value in val_y:
-        letter = [0 for _ in range(6)]
-        letter[int(value)] = 1
-        onehot_encoded.append(letter)
-
-    val_y = np.array(onehot_encoded)
-
-    num_labels = val_y.shape[1]
+    numeric_test_y = test_y
+    print(numeric_test_y.shape)
+    test_y = numeric_to_one_hot(test_y)
 
     current_epoch = 0
 
     while True:
         model.fit(train_x, train_y, batch_size=batch_size, epochs=1)
-        y_pred = model.predict(val_x, batch_size=batch_size)
+        predictions = model.predict(test_x, batch_size=batch_size)
 
-        preds = np.argmax(y_pred, 1)
-        #print(preds)
-        #print(numeric_val_y)
-        result = preds == numeric_val_y
-        sum = np.sum(result)
-        accuracy = sum / float(val_y.shape[0])
+        accuracy = calc_accuracy(predictions, numeric_test_y)
 
-        # Cross Entropy Loss
-        total_loss = 0
-        for j in range(num_labels):
-            loss = log_loss(val_y[:, j], y_pred[:, j])
-            total_loss += loss
+        # Cross Entropy Loss on the test set
+        total_loss = np.mean(-test_y * np.log(predictions))
 
-        total_loss /= num_labels
-
-        print("Epoch: {0} loss: {1} best_loss: {2} validation accuracy: {3}".format(current_epoch, total_loss, best_loss, accuracy))
-
+        # Check if the train should continue and save
         current_epoch += 1
         if total_loss < best_loss or best_loss == -1:
             best_loss = total_loss
@@ -66,6 +70,8 @@ def train_model(model, batch_size, train_x, train_y, val_x, val_y):
         else:
             if current_epoch - best_epoch == patience:
                 break
+
+        print("Epoch: {0} loss: {1} best_loss: {2} validation accuracy: {3}".format(current_epoch, total_loss, best_loss, accuracy))
 
     model.set_weights(best_weights)
     return model
@@ -87,7 +93,7 @@ def get_model(embedding_matrix, sequence_length, dropout_rate, recurrent_units, 
 
     capsule = Flatten()(capsule)
     capsule = Dropout(dropout_p)(capsule)
-    output = Dense(6, activation='softmax')(capsule)
+    output = Dense(number_classes, activation='softmax')(capsule)
     model = Model(inputs=input1, outputs=output)
     model.compile(
         loss='sparse_categorical_crossentropy',
@@ -95,3 +101,5 @@ def get_model(embedding_matrix, sequence_length, dropout_rate, recurrent_units, 
         metrics=['accuracy'])
 
     return model
+
+
